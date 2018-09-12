1c29da75189492ff0de0bb897848060a9ffc5749
faramarz sadeghi <faramarz.sadeghi89@gmail.com>
2018-09-12 11:57:52 +0430

    add loader

--- src/Views/create_modal.blade.php
+++ src/Views/create_modal.blade.php
@@ -49,7 +49,7 @@
         var src = $(this).attr('data-href');
         var iframe = $('#{{LFM_CheckFalseString($section)}}_iframe');
         iframe.contents().find("body").html('');
-        iframe.contents().find("body").html(generate_loader_html('@lang('filemanager.please_wait')'));
+        iframe.contents().find("body").html(lfm_generate_loader_html('@lang('filemanager.please_wait')'));
         iframe.attr("src",src);
         if ({{LFM_CheckFalseString($section)}}_available > 0)
         {
@@ -78,5 +78,23 @@
         $('.modal-backdrop').removeClass();
     }
     //------------------------------------------------------------------------------------//
+    function lfm_generate_loader_html(loading_text) {
+        var loader_html = '' +
+            '<div class="total_loader">' +
+            '   <div class="total_loader_content" style="">' +
+            '       <div class="spinner_area">' +
+            '           <div class="spinner_rects">' +
+            '               <div class="rect1"></div>' +
+            '               <div class="rect2"></div>' +
+            '               <div class="rect3"></div>' +
+            '               <div class="rect4"></div>' +
+            '               <div class="rect5"></div>' +
+            '           </div>' +
+            '       </div>' +
+            '       <div class="text_area">' + loading_text + '</div>' +
+            '   </div>' +
+            '</div>';
+        return loader_html;
+    }
 
 </script>
