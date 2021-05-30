38748214e37dff3a71ac2086fe7ff78aa6365662
faramarz <faramarz.sadeghi89@gmail.com>
2021-05-30 12:15:52 +0430

    dd

--- src/Helpers/Functions/FileManager.php
+++ src/Helpers/Functions/FileManager.php
@@ -980,7 +980,7 @@ function LFM_CreateModalFileManager($section, $options = false, $insert = 'inser
     return $result;
 }
 
-function LFM_CreateModalUpload($section, $callback = 'show_upload_file', $options = [], $result_area_id = false, $modal_id = 'UploadFileManager', $header = 'Upload_FileManager', $button_id = 'ShowModalUpload', $button_content = 'Upload', $button_class = 'btn-default', $font_button_class = false)
+function LFM_CreateModalUpload($section, $callback = 'show_upload_file', $options = [], $result_area_id = false, $modal_id = 'UploadFileManager', $header = 'Upload_FileManager', $button_id = 'ShowModalUpload', $button_content = 'Upload', $button_class = 'btn-default', $font_button_class = false, $no_json_encode = false)
 {
     $session = LFM_SetSessionOption($section, $options, config('laravel_file_manager.upload_route_prefix'));
     $available = LFM_CheckAllowInsert($section)['available'];
@@ -993,7 +993,7 @@ function LFM_CreateModalUpload($section, $callback = 'show_upload_file', $option
     $result['script'] = view("laravel_file_manager::upload.create_uplod_modal_script", compact("src", "modal_id", 'category_id', 'header', 'button_content', 'section', 'callback', 'button_id', 'available', 'result_area_id', 'options'))->render();
     $result['button'] = '<button type="button" class="btn ' . $button_class . '"  id="' . $button_id . '" data-toggle="modal" data-href="' . $src . '"> <i class="' . $font_button_class . '"></i>' . $button_content . '</button>';
     $result['src'] = $src;
-    $result['json'] = json_encode($json);
+    $result['json'] = $no_json_encode ? $json : json_encode($json);
 
     return $result;
 }
